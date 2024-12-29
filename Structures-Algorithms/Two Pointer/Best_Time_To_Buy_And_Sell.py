36cc0caec2152fdb82d23f8ced4d3662832a1dfa
Brandon lyons <brandonlyons@Brandons-MacBook-Pro.local>
2024-12-28 23:06:13 -0500

    Stock question and fixes

---
+++ Structures-Algorithms/Two Pointer/Best_Time_To_Buy_And_Sell.py
@@ -0,0 +1,15 @@
+#Leetcode 1 Two sum, find the two sums that add up to the target, assume you hav only one solution and return the indexes
+nums = [2, 7, 11, 15]
+target = 9
+print(nums)
+
+class Solution:
+    def  twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} # val:index
+
+        for i, n in enumerate(nums):
+            diff  = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return  
