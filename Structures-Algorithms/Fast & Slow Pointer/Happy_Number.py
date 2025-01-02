bfd9bb6bbc1275ebf81d367385523a450e0a67e7
Brandon lyons <blyons2019@fau.edu>
2025-01-01 22:13:16 -0500

    Happy number fast solution

--- Structures-Algorithms/Fast & Slow Pointer/Happy_Number.py
+++ Structures-Algorithms/Fast & Slow Pointer/Happy_Number.py
@@ -4,7 +4,7 @@
 n = 19
 print(n)
  # If a number is visited more than once then it is a loop and not happy
-
+ # T = O(n)
 class Solution:
     def isHappy(self, n: int) -> bool:
         visit = set()
@@ -28,3 +28,24 @@ class Solution:
             n = n // 10
         return output
 
+# fast amd slow pointer solution
+# T = O(logn)  M = O(1)
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        slow = self.nextNumber(n)
+        fast = self.nextNumber(self.nextNumber(n))
+     
+        while slow != fast:
+            if fast == 1: return True
+            slow = self.nextNumber(slow)
+            fast = self.nextNumber(self.nextNumber(fast))
+
+    def nextNumber(self, n: int) -> int: # Caluculating the next number with mod and division
+        output = 0
+
+        while n:
+            digit = n % 10
+            digit = digit ** 2 
+            output += digit
+            n = n // 10
+        return output
