a50b6bd868e261d415ca9c3738043d1f34532e38
Brandon lyons <blyons2019@fau.edu>
2025-01-09 23:05:52 -0500

    Added more comments

--- Structures-Algorithms/Two Pointer/Two_Sum_II.py
+++ Structures-Algorithms/Two Pointer/Two_Sum_II.py
@@ -8,13 +8,13 @@ class Solution:
         l, r = 0, len(numbers) - 1  # left pointer starts at the beggining, right pointer startes at the end
 
         while l < r:  # if the nuber is bigger than the target, move the right pointer left, if it is smaller do the reverse
-            curSum = numbers[l] + numbers[r]
+            curSum = numbers[l] + numbers[r] # Current sum of the two pointers
 
-            if curSum > target:
+            if curSum > target:  # if sum is greater than target move the right pointer the lower the sum
                 r -= 1
-            elif curSum < target:
+            elif curSum < target: # if sum is smaller than target move the left pointer the increase the sum
                 l += 1
             else:
-                return [l + 1, r + 1]
+                return [l + 1, r + 1]  # current sum is equal to the target, add one to each indices to adhere to the question
         return[]
     # linear time solution O(n)
